8b455b0ddfd839e0638ae91fbe6157dd75c24470
lshapz <laurashapiro01@gmail.com>
2017-05-07 19:08:03 -0400

    I think this works

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,24 @@
-function UserController() {
-	this.username = '';
+function UserController($scope) {
+	var ctrl = this
+  ctrl.username = '';
 
-	this.country = {
+	ctrl.country = {
 		name: {
 
 		}
 	};
+    $scope.$watchCollection(function () {
+        return ctrl.username;
+    }, function (newValue, oldValue) {
+        console.log('value updated!');
+    });
+
+    $scope.$watch(function () {
+        return ctrl.country;
+    }, function (newValue, oldValue) {
+        console.log('value updated!');
+    }, true);
+
 }
 
 angular
